01b8a888258ec3337834876c0c717c0feebae377
xiaohang <972382409@qq.com>
2017-01-14 02:39:45 +0800

    编写拦截器，并进行配置

---
+++ src/main/java/com/nfmedia/springmvc/interceptor/SecondInterceptor.java
@@ -0,0 +1,34 @@
+package com.nfmedia.springmvc.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Description
+ * <p>
+ * Author rabbit.
+ * Datetime 2017/1/10.
+ */
+public class SecondInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o)
+            throws Exception {
+        System.out.println("SecondInterceptor preHandle....");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                           Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("SecondInterceptor postHandle....");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                Object o, Exception e) throws Exception {
+        System.out.println("SecondInterceptor afterCompletion....");
+    }
+}
